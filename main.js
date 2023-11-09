957bf4b5f5723f16aa61f3a8e55b5570301b2abb
misrar09 <misrar09@gmail.com>
2023-11-09 18:00:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Create 5 random numbers and print them in DOM
+// Assign 30 sec to those number to disappear
+// Create 5 prompts so in each prompt user has to insert those numbers
+// Check if te inserted numbers are correct 
+
+
+
+/* const userInput = prompt("insert the number") */
+
